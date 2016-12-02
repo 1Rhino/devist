a4116906590a88b758e218bc7ab2c22fdc81e45d
kan <khanh@hipvan.com>
2016-12-02 22:25:21 +0700

    Initial Commit

---
+++ devist/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  devist
+//
+//  Created by bui khanh on 2/12/16.
+//  Copyright © 2016 kan bui. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
